6ab1a73a14c194df3eb19e298816463449906b62
orxan.m <99455@ORXAN>
2024-01-08 17:38:33 +0400

    add repositories part 1

---
+++ src/Api/Core/BlazorSozluk.Api.Application/Abstract/Repositories/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorSozluk.Api.Application.Abstract.Repositories;
+
+public interface IUserRepository
+{
+}
